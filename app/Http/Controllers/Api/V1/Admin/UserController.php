4059f9d647cc68be449cbc4cda2157279784e56b
abbass <abbas.ajorlou1371@gmail.com>
2024-05-06 11:11:41 +0330

    chore: Add UserPolicy for authorization control

--- app/Http/Controllers/Api/V1/Admin/UserController.php
+++ app/Http/Controllers/Api/V1/Admin/UserController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(User::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
