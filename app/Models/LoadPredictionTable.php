d38d9016f8fd0114c4a2bad32e298147b6a9b025
abbass <abbas.ajorlou1371@gmail.com>
2024-10-24 17:20:32 +0330

    Load prediction is done

---
+++ app/Models/LoadPredictionTable.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LoadPredictionTable extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array[]
+     */
+    protected $fillable = ['crop_type_id', 'headers', 'rows'];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected function casts()
+    {
+        return [
+            'headers' => 'array',
+            'rows' => 'array',
+        ];
+    }
+
+    /**
+     * The attributes with default values.
+     *
+     * @var array<string, mixed>
+     */
+    protected $attributes = [
+        'headers' => '[
+            "شرایط باغ",
+            "وزن خوشه میوه در وضعیت های مختلف",
+            "میانگین تعداد جوانه",
+            "تبدیل جوانه گل به خوشه میوه",
+            "ضریب تبدیل میزان محصول تخمینی به میزان محصول واقعی",
+            "وزن بار درخت (گرم)",
+            "وزن بار درخت (کیلوگرم)",
+            "تعداد درخت",
+            "وزن کل بار باغ (کیلوگرم)"
+            ]',
+        'rows' => '[
+            ["Excelent", "67", "0.89", "0.92", "0", "0", "0", "0", "0"],
+            ["Good", "57", "0.89", "0.92", "0", "0", "0", "0", "0"],
+            ["Normal", "51", "0.89", "0.92", "0", "0", "0", "0", "0"],
+            ["Bad", "39.3", "0.89", "0.92", "0", "0", "0", "0", "0"]]',
+    ];
+
+    /**
+     * Get the crop type that owns the load prediction table.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function cropType()
+    {
+        return $this->belongsTo(CropType::class);
+    }
+}
