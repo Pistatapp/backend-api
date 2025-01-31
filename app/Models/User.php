824596241b417904c82609ac5d1cc9bfbc768ece
abbass <abbas.ajorlou1371@gmail.com>
2025-01-31 17:07:18 +0330

    Add Algolia Scout Extended package and make User model searchable; remove AuthControllerTest

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Scout\Searchable;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
@@ -13,6 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable implements HasMedia
 {
     use HasApiTokens, HasFactory, Notifiable, InteractsWithMedia, HasRoles;
+    use Searchable;
 
     /**
      * The attributes that are mass assignable.
