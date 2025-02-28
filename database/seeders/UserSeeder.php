eae9ddddbc22bc3ed20cf68e99bd215b2e2f7feb
abbass <abbas.ajorlou1371@gmail.com>
2025-02-28 13:54:49 +0330

    Update UserSeeder to create an additional user and assign roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,11 +13,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $users = \App\Models\User::factory()
-            ->count(2)
+            ->count(3)
             ->has(\App\Models\Profile::factory())
             ->sequence(
                 ['mobile' => '09107529334'],
-                ['mobile' => '09369238614']
+                ['mobile' => '09369238614'],
+                ['mobile' => '09195065248'],
             )
             ->create();
 
@@ -26,6 +27,7 @@ class UserSeeder extends Seeder
         ]);
 
         $users->where('mobile', '09107529334')->first()->assignRole('root');
+        $users->where('mobile', '09195065248')->first()->assignRole('root');
         $users->where('mobile', '09369238614')->first()->assignRole('admin');
     }
 }
