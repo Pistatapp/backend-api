e714d6a5943b2276d4322d0a102204f4482debbe
abbass <abbas.ajorlou1371@gmail.com>
2025-03-01 09:25:58 +0330

    Small bug fix

--- app/Jobs/CalculateFrostbiteRiskJob.php
+++ app/Jobs/CalculateFrostbiteRiskJob.php
@@ -21,7 +21,7 @@ class CalculateFrostbiteRiskJob implements ShouldQueue
      */
     public function handle()
     {
-        $farms = Farm::with('frostbitRisks', 'user')->get();
+        $farms = Farm::with('frostbitRisks', 'users')->get();
 
         foreach ($farms as $farm) {
             foreach ($farm->frostbitRisks as $risk) {
