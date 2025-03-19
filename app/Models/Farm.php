01b8b4bd77c851e99a5aae4c906b93f0ee141a37
abbass <abbas.ajorlou1371@gmail.com>
2025-03-19 10:02:37 +0330

    Small bug fix

--- app/Models/Farm.php
+++ app/Models/Farm.php
@@ -44,7 +44,7 @@ class Farm extends Model
      */
     public function isWorkingEnvironment()
     {
-        return $this->id === auth()->user()->preferences['working_environment'] ?? false;
+        return $this->id === auth()->user()?->preferences['working_environment'] ?? false;
     }
 
     /**
