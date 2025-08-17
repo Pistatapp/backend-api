3b5057034f5852f5bc5aba5037e6948428206d70
abbass <abbas.ajorlou1371@gmail.com>
2025-08-17 14:22:45 +0330

    Small bug fix

--- app/Policies/DriverPolicy.php
+++ app/Policies/DriverPolicy.php
@@ -21,7 +21,7 @@ class DriverPolicy
      */
     public function view(User $user, Driver $driver): bool
     {
-        return $driver->farm->admin->is($user);
+        return $driver->farm->admins->contains($user);
     }
 
     /**
@@ -37,7 +37,7 @@ class DriverPolicy
      */
     public function update(User $user, Driver $driver): bool
     {
-        return $driver->farm->admin->is($user);
+        return $driver->farm->admins->contains($user);
     }
 
     /**
@@ -45,7 +45,7 @@ class DriverPolicy
      */
     public function delete(User $user, Driver $driver): bool
     {
-        return $driver->farm->admin->is($user);
+        return $driver->farm->admins->contains($user);
     }
 
     /**
