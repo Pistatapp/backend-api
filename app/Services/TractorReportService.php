40703dd4c82d4f2e2ae4c3fdfee0d8f94857ac8c
abbass <abbas.ajorlou1371@gmail.com>
2025-08-25 02:28:24 +0330

    Small bug fix

--- app/Services/TractorReportService.php
+++ app/Services/TractorReportService.php
@@ -91,7 +91,7 @@ class TractorReportService
         return [
             'id' => $tractor->id,
             'name' => $tractor->name,
-            'speed' => $reports->avg('speed'),
+            'speed' => $reports->avg('speed') ?? 0,
             'status' => $reports->last()->status ?? 0,
             'start_working_time' => $startWorkingTime ? $startWorkingTime->date_time->format('H:i:s') : '00:00:00',
             'traveled_distance' => number_format($dailyReport->traveled_distance ?? 0, 2),
