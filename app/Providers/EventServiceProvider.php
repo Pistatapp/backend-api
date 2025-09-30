7d2139e033416a7b93fbd8f6d654ef558c6f535f
abbass <abbas.ajorlou1371@gmail.com>
2025-09-30 16:31:20 +0330

    Small bug fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,8 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Broadcast;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -26,12 +24,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Broadcast::routes([
-            'prefix' => 'api',
-            'middleware' => ['api', 'auth:sanctum'],
-        ]);
-
-        require base_path('routes/channels.php');
+        //
     }
 
     /**
