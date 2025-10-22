66a9899b4b64996963a848a4838ff506fb5ecc5c
abbass <abbas.ajorlou1371@gmail.com>
2025-10-22 23:55:16 +0330

    Small bug fix

--- app/Http/Resources/ActiveTractorResource.php
+++ app/Http/Resources/ActiveTractorResource.php
@@ -18,8 +18,8 @@ class ActiveTractorResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'status' => $this->is_working,
-            'start_working_time' => $this->calculated_start_work_time ?? $this->start_work_time ?? '08:00:00',
-            'end_working_time' => $this->calculated_end_work_time ?? $this->end_work_time ?? '17:00:00',
+            'start_working_time' => $this->calculated_start_work_time,
+            'end_working_time' => $this->calculated_end_work_time,
             'on_time' => $this->on_time,
         ];
     }
