91c121281a44ce2d48ce83cd8a549c6f09de7fe2
abbass <abbas.ajorlou1371@gmail.com>
2025-10-26 21:33:44 +0330

    Add loggin for testing purposes

--- app/Http/Controllers/Api/V1/Tractor/GpsReportController.php
+++ app/Http/Controllers/Api/V1/Tractor/GpsReportController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use App\Events\TractorStatus;
 use App\Events\ReportReceived;
+use Illuminate\Support\Facades\Log;
 
 class GpsReportController extends Controller
 {
@@ -26,6 +27,7 @@ class GpsReportController extends Controller
     {
         try {
             $rawData = $request->getContent();
+            Log::info('Raw data: ' . $rawData);
             $data = $this->parseDataService->parse($rawData);
 
             $deviceImei = $data[0]['imei'];
