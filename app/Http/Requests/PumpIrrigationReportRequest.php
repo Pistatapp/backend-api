b70f9dd72fac879ec17b385318bdf04687673937
abbass <abbas.ajorlou1371@gmail.com>
2025-11-11 12:31:38 +0330

    Small bug fix made

--- app/Http/Requests/PumpIrrigationReportRequest.php
+++ app/Http/Requests/PumpIrrigationReportRequest.php
@@ -22,16 +22,8 @@ class PumpIrrigationReportRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'start_date' => ['required', 'string', function ($attribute, $value, $fail) {
-                if (!is_jalali_date($value)) {
-                    $fail('The ' . $attribute . ' must be a valid Jalali date in Y/m/d format.');
-                }
-            }],
-            'end_date' => ['required', 'string', function ($attribute, $value, $fail) {
-                if (!is_jalali_date($value)) {
-                    $fail('The ' . $attribute . ' must be a valid Jalali date in Y/m/d format.');
-                }
-            }],
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
         ];
     }
 
