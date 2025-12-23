b49a5ff3705b06dd8f43c497eb64d8614074bc40
abbass <abbas.ajorlou1371@gmail.com>
2025-12-23 11:40:29 +0330

    Remove redundant string validation rule for date parameters in UpdateWarningRequest, streamlining the validation logic for Jalali date format.

--- app/Http/Requests/UpdateWarningRequest.php
+++ app/Http/Requests/UpdateWarningRequest.php
@@ -110,7 +110,6 @@ class UpdateWarningRequest extends FormRequest
         // Date parameters - validate Jalali date format
         if (in_array($param, ['start_date', 'end_date', 'date'])) {
             return [
-                'string',
                 function ($attribute, $value, $fail) {
                     if (!is_jalali_date($value)) {
                         $fail('The ' . $attribute . ' must be a valid Jalali date in format YYYY/MM/DD (e.g., 1403/01/15).');
