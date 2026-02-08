319196dcaa2ed23f81c82b70d9903e88e15e2ee1
abbass <abbas.ajorlou1371@gmail.com>
2026-02-09 00:16:37 +0330

    Small bug fix made

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -39,7 +39,6 @@ class UserResource extends JsonResource
             'last_activity_at' => jdate($this->last_activity_at)->format('Y/m/d H:i:s'),
             'profile' => new ProfileResource($this->whenLoaded('profile')),
             'role' => $role,
-            'farms' => FarmResource::collection($this->whenLoaded('farms')),
             $this->mergeWhen($role === 'labour', [
                 'labour' => new LabourResource($this->whenLoaded('labour')),
             ]),
