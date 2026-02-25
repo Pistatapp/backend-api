b71cdd0079d707ff18f6915c54c20e1c3604fdd9
abbass <abbas.ajorlou1371@gmail.com>
2026-02-25 18:36:25 +0330

    Remove unused sanitizeUtf8 method from GpsReportController and update prepareBatch method in StoreGpsData job to decode JSON strings into arrays. This streamlines data handling and improves type safety.

--- app/Http/Controllers/Api/V1/Tractor/GpsReportController.php
+++ app/Http/Controllers/Api/V1/Tractor/GpsReportController.php
@@ -17,20 +17,4 @@ class GpsReportController extends Controller
 
         return response()->json(['success' => true], 200);
     }
-
-    private function sanitizeUtf8(string $data): string
-    {
-        if (mb_check_encoding($data, 'UTF-8')) {
-            return $data;
-        }
-
-        $sanitized = mb_convert_encoding($data, 'UTF-8', 'UTF-8');
-
-        if ($sanitized === false) {
-            $sanitized = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]/u', '', $data);
-            $sanitized = iconv('UTF-8', 'UTF-8//IGNORE', $sanitized);
-        }
-
-        return $sanitized ?: '';
-    }
 }
