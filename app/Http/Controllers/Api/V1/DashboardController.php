9c461344f6a08fcc87c0d459aa75c6335c03538c
abbass <abbas.ajorlou1371@gmail.com>
2026-05-15 09:57:36 +0330

    Enhance weather data retrieval with error handling
    - Wrapped the weather data fetching logic in a try-catch block to handle potential exceptions gracefully.
    - Added default return values for weather data in case of an error, ensuring the application remains stable and provides consistent responses.

--- app/Http/Controllers/Api/V1/DashboardController.php
+++ app/Http/Controllers/Api/V1/DashboardController.php
@@ -85,17 +85,30 @@ class DashboardController extends Controller
      */
     private function getWeatherData($location)
     {
-        $weatherData = weather_api()->current($location);
+        try {
+            $weatherData = weather_api()->current($location);
 
-        return [
-            'last_updated' => jdate($weatherData['current']['last_updated'])->format('Y/m/d H:i:s'),
-            'temp_c' => number_format($weatherData['current']['temp_c'], 2),
-            'condition' => $weatherData['current']['condition']['text'],
-            'icon' => $weatherData['current']['condition']['icon'],
-            'wind_kph' => number_format($weatherData['current']['wind_kph'], 2),
-            'humidity' => number_format($weatherData['current']['humidity'], 2),
-            'dewpoint_c' => number_format($weatherData['current']['dewpoint_c'], 2),
-            'cloud' => number_format($weatherData['current']['cloud'], 2),
-        ];
+            return [
+                'last_updated' => jdate($weatherData['current']['last_updated'])->format('Y/m/d H:i:s'),
+                'temp_c' => number_format($weatherData['current']['temp_c'], 2),
+                'condition' => $weatherData['current']['condition']['text'],
+                'icon' => $weatherData['current']['condition']['icon'],
+                'wind_kph' => number_format($weatherData['current']['wind_kph'], 2),
+                'humidity' => number_format($weatherData['current']['humidity'], 2),
+                'dewpoint_c' => number_format($weatherData['current']['dewpoint_c'], 2),
+                'cloud' => number_format($weatherData['current']['cloud'], 2),
+            ];
+        } catch (\Throwable $e) {
+            return [
+                'last_updated' => null,
+                'temp_c' => 0,
+                'condition' => null,
+                'icon' => null,
+                'wind_kph' => 0,
+                'humidity' => 0,
+                'dewpoint_c' => 0,
+                'cloud' => 0,
+            ];
+        }
     }
 }
