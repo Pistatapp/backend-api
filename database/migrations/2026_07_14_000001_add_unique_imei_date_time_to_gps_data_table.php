8ac906e26be33d2ea662ba68e045a53abccb69f2
Hooshekar <hooshekar@gmail.com>
2026-07-31 02:54:09 +0330

    deploy

---
+++ database/migrations/2026_07_14_000001_add_unique_imei_date_time_to_gps_data_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddUniqueImeiDateTimeToGpsDataTable extends Migration
+{
+    public function up(): void
+    {
+        // عملیات پاکسازی و ایندکس قبلاً به صورت دستی انجام شده است.
+    }
+
+    public function down(): void
+    {
+        //
+    }
+}
